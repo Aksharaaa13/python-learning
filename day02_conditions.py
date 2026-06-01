eb1faccf9d4d1a912fefaba26d07ccce7c343122
Dasari Akshara <se25uece011@mahindrauniversity.edu.in>
2026-06-01 19:44:14 +0530

    Add files via upload

---
+++ day02_conditions.py
@@ -0,0 +1,17 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # Day 2 - Conditions
+... 
+... number = int(input("Enter a number: "))
+... 
+... if number % 2 == 0:
+...     print("Even")
+... else:
+...     print("Odd")
+... 
+... 
+... marks = int(input("Enter marks: "))
+... 
+... if marks >= 40:
+...     print("PASS")
+... else:
